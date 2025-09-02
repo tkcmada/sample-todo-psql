44897f1fec4e5c30b788c57710ddefe717de93d8
tkcmada <tokutaro@gmail.com>
2025-09-03 07:27:11 +0900

    feat: introduce todo service layer

--- src/server/api/routers/todo.ts
+++ src/server/api/routers/todo.ts
@@ -1,33 +1,33 @@
 import { createTRPCRouter, publicProcedure } from '../trpc';
-import { todoRepository } from '@/server/repositories/todoRepository';
+import { todoService } from '@/server/services/todoService';
 import { createTodoSchema, updateTodoSchema, deleteTodoSchema, toggleTodoSchema } from '@/lib/validations';
 
 export const todoRouter = createTRPCRouter({
   getAll: publicProcedure.query(async () => {
-    return await todoRepository.getAll();
+    return await todoService.getAll();
   }),
 
   create: publicProcedure
     .input(createTodoSchema)
     .mutation(async ({ input }) => {
-      return await todoRepository.create(input);
+      return await todoService.create(input);
     }),
 
   update: publicProcedure
     .input(updateTodoSchema)
     .mutation(async ({ input }) => {
-      return await todoRepository.update(input);
+      return await todoService.update(input);
     }),
 
   delete: publicProcedure
     .input(deleteTodoSchema)
     .mutation(async ({ input }) => {
-      return await todoRepository.delete(input.id);
+      return await todoService.delete(input.id);
     }),
 
   toggle: publicProcedure
     .input(toggleTodoSchema)
     .mutation(async ({ input }) => {
-      return await todoRepository.toggle(input.id);
+      return await todoService.toggle(input.id);
     }),
 });
